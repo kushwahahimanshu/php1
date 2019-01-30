d20e614823ffa8f04951c84644f81e3f73b453b9
kushwahahimanshu <himanshu0025kushwaha@gmail.com>
2019-01-30 15:37:45 +0530

    we add extra file

---
+++ layout/extra.php
@@ -0,0 +1,13 @@
+<?php 
+  $a= 10;
+  $b= 20;
+  if ($a>=$b) {
+  	echo "a is greater than b ";
+  }
+  elseif ($a==$b) {
+    echo "a is equal to b";
+  }
+  else{
+  	echo "a is less than b";
+  }
+ ?>
