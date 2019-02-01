9b18150e936dfa1098f86cc7c1d868a3893e8397
kushwahahimanshu <himanshu0025kushwaha@gmail.com>
2019-02-01 13:31:41 +0530

    all file

---
+++ test/test1.php
@@ -0,0 +1,9 @@
+<?php 
+$username="himanshu";
+$password="hk123";
+if (username=='himanshu' &&  password=='hk123') {
+    echo "you are login";
+}
+else{
+	echo "username and password invalid";
+ ?>}
